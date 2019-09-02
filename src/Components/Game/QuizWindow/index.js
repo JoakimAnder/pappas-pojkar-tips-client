63610ed4f17e027e8d913e874b22c8a57fe63511
Joakim Andersson <Joakim.Ander@Hotmail.com>
2019-09-02 14:37:23 +0200

    added parseState
    to quizwindow

--- src/Components/Game/QuizWindow/index.js
+++ src/Components/Game/QuizWindow/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const QuizWindow = () => {
+    const [quiz, setQuiz] = useState("groupstage");
+    function parseState() {
+        switch(quiz) {
+            case "test": return <div />;
+            default: return <div />;
+        }
+    }
     return (
         <div>
             {/*tabs*/}
             {/*quiz*/}
+            {parseState()}
         </div>
     );
 };
